e9464f8e8ec8b8bd802b6c9c261fab708b0e5faf
Baro027 <baran.yildirim@student.fh-kiel.de>
2024-12-18 19:45:58 +0100

    Big Kommis

--- ui_manager.py
+++ ui_manager.py
@@ -1,6 +1,8 @@
 
 import pygame
 from character_data import characters
+
+
 class UIManager:
     def __init__(self, screen, fonts, colors, draw_text,maps):
         self.screen = screen
@@ -10,33 +12,15 @@ class UIManager:
         self.maps = maps
         self.current_map = None
         
-        
-        # Ressourcen zentral laden
-        #self.bg_image = pygame.image.load("Hintergrund/Fate's Moon.png")
-        self.current_map = None
-        
-        #zeichnet die Profilbilder der fighter unter die Healthbars
-        """""
-        self.character_images = {
-            "Zuko": pygame.image.load("Zuko/zuko_pb.png"),
-            "Susanoo": pygame.image.load("Susanoo/susanoo_pb.png"),
-            "Basim": pygame.image.load("Basim/basim_pb_.png"),
-            "Mai": pygame.image.load("Mai/mai_pb.png"),
-            
-        }
-        """
-
+       
+    # Setzt die aktuelle Map basierend auf dem Namen
     def set_map(self, map_name):
-        """
-        Setzt die aktuelle Map basierend auf dem Namen.
-        :param map_name: Der Name der Map, die gesetzt werden soll.
-        """
         if map_name in self.maps:
             self.current_map = self.maps[map_name]
         
 
 
-
+    # Health_bar ebenfalls von dem YouTube Video aus der Champion Class insipiriert bzw. die Logik mit Ratio
     def draw_health_bar(self, health, x, y):
         hb_width = 400
         hb_height = 30
@@ -64,15 +48,10 @@ class UIManager:
 
     def draw_champion_profilepicture(self, character_name, x, y):
         #Zeichnet das Profilbild des ausgewählten Charakters.
-        #profile_image = self.character_images.get(character_name)
 
         # Hole das Profilbild aus dem zentralen characters-Dictionary
         profile_image = characters.get(character_name, {}).get("loaded_profile_picture")
 
-        # Sicherheitscheck: Wenn Bild nicht existiert
-        if not profile_image:
-            print(f"Fehler: Kein Bild für Charakter {character_name} gefunden.")
-            return
     
         frame_width, frame_height = 54, 54
         profile_width, profile_height = 50, 50
@@ -94,12 +73,11 @@ class UIManager:
         name2_width = name_font.size(fighter_2_data["attributes"]["name"])[0]
 
         # Dynamische Positionen für die Namen
-        #Hier an den Paramtern können die Namen alligned werden
         name1_x = 116 - name1_width // 2  # Zentriere unter Mana-Balken 1
         name2_x = 870 - name2_width // 2  # Zentriere unter Mana-Balken 2
 
         # Namen zeichnen
-        self.draw_text(fighter_1_data["attributes"]["name"], name_font, self.colors["WHITE"], name1_x, 90)  # Y-Offset für Namen
+        self.draw_text(fighter_1_data["attributes"]["name"], name_font, self.colors["WHITE"], name1_x, 90)  
         self.draw_text(fighter_2_data["attributes"]["name"], name_font, self.colors["WHITE"], name2_x, 90)
 
 
